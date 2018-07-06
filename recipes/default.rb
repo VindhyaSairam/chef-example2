c9a94f6d06297432cffe5376d9e23011fb0ef5cf
vindhya <vindhya.china@gmail.com>
2018-07-06 19:14:08 +0000

    added a new template file and modified the default recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,14 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+#
+package 'apache2' 
+
+service 'apache2' do
+    service_name 'apache2'
+      action [:enable,:start]
+end
+
+template '/var/www/html/index.html' do
+  source 'index.html.erb'
+end
